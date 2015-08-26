17a3c102d4e4dcb64e7a9a389ab856c5340814f3
JonneyDai <jonneydai@126.com>
2015-08-26 17:43:10 +0800

    添加文件

---
+++ HandleDevice/HandleDevice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HandleDevice
+//
+//  Created by 代隽杰 on 15/8/26.
+//  Copyright (c) 2015年 代隽杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HandleDevice/HandleDevice/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HandleDevice
+//
+//  Created by 代隽杰 on 15/8/26.
+//  Copyright (c) 2015年 代隽杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
