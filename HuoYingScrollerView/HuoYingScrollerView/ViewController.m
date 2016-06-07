3630eda8a55f1bb02266490ec04e6dc5da62deab
JonneyDai <jonneydai@126.com>
2016-06-07 23:15:27 +0800

    add

--- HuoYingScrollerView/HuoYingScrollerView/ViewController.m
+++ HuoYingScrollerView/HuoYingScrollerView/ViewController.m
@@ -13,10 +13,10 @@ CGFloat kImgCount = 10;
 CGFloat scrollY = 20;
 CGFloat pageCtrlWidth = 200;
 
-@interface ViewController ()
+@interface ViewController ()<UIScrollViewDelegate>
 @property (nonatomic ,strong) UIScrollView *scrollView;
 @property (nonatomic ,strong) UIPageControl *pageCtrl;
-
+@property (nonatomic ,strong) 
 
 @end
 
@@ -25,12 +25,35 @@ CGFloat pageCtrlWidth = 200;
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self initSrollView];
-}
-
--(void) initSrollView{
-    self.scrollView = [[UIScrollView alloc]initWithFrame:CGRectMake(0, scrollY, kScreenWidth, kScreenHeight)];
+    [self initPageCtrl];
     
 }
+//创建ScrollView
+-(void) initSrollView{
+    self.scrollView = [[UIScrollView alloc]initWithFrame:CGRectMake(0, scrollY, kScreenWidth, kScreenHeight - scrollY)];
+    self.scrollView.contentSize = CGSizeMake(kScreenWidth * kImgCount, kScreenHeight - scrollY);
+    for (int i = 0; i <= kImgCount; i++) {
+        UIImageView *imageView = [[UIImageView alloc]initWithFrame:CGRectMake(i *kScreenWidth, scrollY, kScreenWidth, kScreenHeight - scrollY)];
+        UIImage *myImage = [UIImage imageNamed:[NSString stringWithFormat:@"huoying%d",i + 1] ];
+        imageView.image = myImage;
+        [self.scrollView addSubview:imageView];
+    }
+    
+    self.scrollView.pagingEnabled = YES;
+    self.scrollView.delegate = self;
+    [self.view addSubview: self.scrollView];
+    
+}
+//创建UIPageCtrl
+-(void) initPageCtrl{
+    self.pageCtrl = [[UIPageControl alloc]initWithFrame:CGRectMake((kScreenWidth - pageCtrlWidth)/2, kScreenHeight - scrollY,  pageCtrlWidth, scrollY)];
+    self.pageCtrl.numberOfPages = kImgCount;
+    self.pageCtrl.pageIndicatorTintColor = [UIColor whiteColor];
+    self.pageCtrl.currentPageIndicatorTintColor = [UIColor lightGrayColor];
+    [self.view insertSubview:self.pageCtrl aboveSubview:self.scrollView];
+}
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
